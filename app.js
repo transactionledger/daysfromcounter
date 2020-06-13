dcd005ac560b4c94327205ccb1657ebbec2c0436
Jeff <info@transactionledger.dev>
2020-06-13 12:17:20 -0400

    tweak css

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 userbase.init({ appId: '54f1020c-a366-416f-b9ca-3f140b975383' })
-    .then((session) => session.user ? showTodos(session.user.username) : showAuth())
-    .catch(() => showAuth())
-    .finally(() => document.getElementById('loading-view').style.display = 'none')
+    .then((session) => session.user ? showTodos(session.user.username) : showLogin())
+    .catch(() => showSignup())
 
 function loadDateData() {
 
 }
 
-function showAuth() {
-
-}
 
 function handleSignUp(e) {
     e.preventDefault()
